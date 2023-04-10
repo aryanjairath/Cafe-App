dacd71db50e404d06392c733be6de90ac5d0d075
Owner <aryanjairath7@gmail.com>
2023-04-10 18:23:25 -0400

    initial commit

--- app/src/main/java/cs213/project5/MainActivity.java
+++ app/src/main/java/cs213/project5/MainActivity.java
@@ -3,7 +3,7 @@ package cs213.project5;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Testing
+//Testing app
 public class MainActivity extends AppCompatActivity {
 
     @Override
