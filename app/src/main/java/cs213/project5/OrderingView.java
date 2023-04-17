ff48318c56cef47936ec3d7492975e763d56a5e4
Anis C <anis.chihoub@gmail.com>
2023-04-17 14:10:03 -0400

    fix coffee again

--- app/src/main/java/cs213/project5/OrderingView.java
+++ app/src/main/java/cs213/project5/OrderingView.java
@@ -2,13 +2,28 @@ package cs213.project5;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class OrderingView extends AppCompatActivity {
+    private ListView ordersView;
+
+    private ArrayAdapter<String> adapterView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store_orders_layout);
-        //doit();
+
+
+    }
+
+    private void onLoad(){
+
+
+
+
+
     }
 
 }
